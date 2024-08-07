57f0b417e2f39fb41c176af8a2e37d879ec56230
jasmeen1234 <107848048+jasmeen1234@users.noreply.github.com>
2024-08-07 20:11:07 +0530

    Update HomePage.js

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,17 +1,20 @@
-// src/Pages/HomePage.js
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../Components/Sidebar';
 import MovieDetail from './MovieDetail';
 import Header from '../Components/Header';
+import { calcLength } from 'framer-motion';
 
 const HomePage = () => {
+  let [order,setOrder]=useState("");
+  let [starRatings,setStarRatings]=useState([])
   return (
     <div className="min-h-screen bg-gray-300">
       <Header />
       <div className="flex p-1">
-        <Sidebar />
+        <Sidebar order={order} setOrder={setOrder} setStarRatings={setStarRatings} />
+        {console.log(starRatings)}
         <div className="flex-1 p-4">
-          <MovieDetail />
+          <MovieDetail order={order} starRatings={starRatings} />
         </div>
       </div>
     </div>
