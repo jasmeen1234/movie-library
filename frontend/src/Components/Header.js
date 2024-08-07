3fac712ee28549c1c347d7be62f60b056bf2b2a8
jasmeen <jasmeenbano12@gmail.com>
2024-08-07 13:17:08 +0530

    commit

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,14 +1,13 @@
-// src/Components/Header.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="sticky top-0 z-50 bg-blue-400 flex text-white p-4  shadow-md">
-     
+    <div className="sticky top-0 z-50 bg-blue-400 flex text-white p-4 shadow-md">
       <div className="mt-2">
-      <h1 className="text-3xl font-bold">Movie Library</h1>
-        <a href="/home" className="text-white mr-4">Home</a>
-        <a href="/login" className="text-white">Login</a>
+        <h1 className="text-3xl font-bold">Movie Library</h1>
+        <Link to="/home" className="text-white mr-4">Home</Link>
+        <Link to="/login" className="text-white">Login</Link>
       </div>
     </div>
   );
